78477de2bf3d0b3e12fe41bb70f6789a00ab4f53
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-06-01 12:23:22 -0300

    Showing a username and an avatar

--- lib/messengyr_web/views/layout_view.ex
+++ lib/messengyr_web/views/layout_view.ex
@@ -4,4 +4,21 @@ defmodule MessengyrWeb.LayoutView do
   def logged_in?(conn) do
     Guardian.Plug.authenticated?(conn, [])
   end
+
+  def username(conn) do
+    user = Guardian.Plug.current_resource(conn)
+    %{username: username} = user
+
+    username
+  end
+
+  def avatar(conn) do
+    user = Guardian.Plug.current_resource(conn)
+
+    %{email: email} = user
+
+    hash_email = :crypto.hash(:md5, email) |> Base.encode16 |> String.downcase
+
+    "http://www.gravatar.com/avatar/#{hash_email}"
+  end
 end
