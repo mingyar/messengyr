9e03ef615295f5cbacdf72ea928f8325f58089fd
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-06-09 04:13:25 -0300

    Creanting a Chat context

---
+++ lib/messengyr/chat/chat.ex
@@ -0,0 +1,29 @@
+defmodule Messengyr.Chat do
+
+  alias Messengyr.Chat.{Message, Room, RoomUser}
+  alias Messengyr.Repo
+
+  def crate_room do
+    room = %Room{}
+    Repo.insert(room)
+  end
+
+  def add_room_user(room, user) do
+    room_user = %RoomUser{
+      room: room,
+      user: user
+    }
+
+    Repo.insert(room_user)
+  end
+
+  def add_message(%{room: room, user: user, text: text}) do
+    message = %Message{
+      room: room,
+      user: user,
+      text: text
+    }
+
+    Repo.insert(message)
+  end
+end
