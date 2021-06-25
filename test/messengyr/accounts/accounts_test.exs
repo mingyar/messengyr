4b5337a3704e85ae33e44762ccb7da95fbea8d6f
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-06-25 15:04:49 -0300

    Creating 'create_user/1 with valid data creates a user' test

--- test/messengyr/accounts/accounts_test.exs
+++ test/messengyr/accounts/accounts_test.exs
@@ -1,6 +1,7 @@
 defmodule Messengyr.AccountsTest do
   use Messengyr.DataCase
   alias Messengyr.Accounts
+  alias Messengyr.Accounts.User
 
   test "create_user/1 with missing data returns error changeset" do
     params = %{
@@ -9,5 +10,15 @@ defmodule Messengyr.AccountsTest do
     }
 
     assert {:error, %Ecto.Changeset{}} = Accounts.create_user(params)
+  end
+
+  test "create_user/1 with valid data creates a user" do
+    params = %{
+      "username" => "mingyar",
+      "password" => "pa55w0rd",
+      "email" => "mingyar.furtado@gmail.com",
+    }
+
+    assert {:ok, %User{}} = Accounts.create_user(params)
   end
 end
