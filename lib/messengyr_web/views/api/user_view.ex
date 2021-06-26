01026652278c16d9066c5768e45ad73d5b4e15dc
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-06-25 21:05:35 -0300

    Adding doctests

--- lib/messengyr_web/views/api/user_view.ex
+++ lib/messengyr_web/views/api/user_view.ex
@@ -1,6 +1,29 @@
 defmodule MessengyrWeb.UserView do
   use MessengyrWeb, :view
 
+  @doc """
+  Renders a user in JSON format.
+
+
+  ## Example
+
+      iex> alias Messengyr.Accounts.User
+      iex> user = %User{
+      ...>   id: 1,
+      ...>   username: "test",
+      ...>   email: "test@example.com"
+      ...> }
+      iex> MessengyrWeb.UserView.render("show.json", %{user: user})
+      %{
+        user: %{
+          id: 1,
+          username: "test",
+          avatarURL: "http://www.gravatar.com/avatar/55502f40dc8b7c769880b10874abc9d0",
+        }
+      }
+
+
+  """
   def render("show.json", %{user: user}) do
     %{
       user: user_jason(user)
